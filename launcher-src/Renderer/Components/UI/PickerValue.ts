d84e44638cde29a70c7c20548fbae5997d9e62f9
StepnickaSantiago <stepnicka012@gmail.com>
2026-06-15 03:30:00 -0300

    Delete launcher-src directory

--- launcher-src/Renderer/Components/UI/PickerValue.ts
+++
@@ -1,8 +0,0 @@
-export interface PickerValue {
-    getValue: () => string | number;
-    setValue?: (value: string | number) => void;
-    onChange?: (callback: (val: any) => void) => void;
-    pause?: () => void;
-    resume?: () => void;
-    destroy?: () => void;
-}
